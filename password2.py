1d0a9587a7628b439b6d99e499ce8c3ab3841cb0
velkyaguilar <108696461+velkyaguilar@users.noreply.github.com>
2025-11-12 16:36:15 -0600

    Delete password2.py

--- password2.py
+++
@@ -1,8 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-texto = "x?1_p-M.4!eM"
-
-texto_encriptado = generate_password_hash(texto)
-print(f"texto encriptado: {texto_encriptado}")
-
-print(f"¿el texto es corecto? {check_password_hash(texto_encriptado, texto)}")
