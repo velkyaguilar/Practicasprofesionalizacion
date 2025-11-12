0bf095f619126d25821e5c72ad0223d8e67b0619
velkyaguilar <108696461+velkyaguilar@users.noreply.github.com>
2025-11-12 16:36:12 -0600

    Delete password1.py

--- password1.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt 
-
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-
-password_plano = "mi_contraseña_secreta"
-hashed_password = bcrypt.generate_password_hash(password_plano).decode('utf-8') 
-print("Contraseña Hasheada:", hashed_password)
